0e1254f0f65b65b6183cbb7b7555d2d5735a7d36
Angkii30 <angkinan30m@gmail.com>
2025-11-11 20:54:39 +0700

    เพิ่ม  header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import Map from './component/Map'
 import './App.css'
+import logo from './assets/pic1.png'
+import avatar from './assets/pic2.png'
+import { FiAlignJustify } from "react-icons/fi";
 
-// function App() {
-//   return <Map />
-// }
+function App() {
+  return (
+    <>
+      <header className="top-bar">
+        <button className='menu-btn' aria-label='เปิดเมนู'>
+          <FiAlignJustify size={22} />
+        </button>
+
+        <img className="logo" src={logo} alt="Logo" />
+
+        <button className='profile-btn' aria-label='โปรไฟล์'>
+          <img src={avatar} alt="Profile" />
+        </button>
+      </header>
+      {/* <Map /> */}
+    </>
+  )
+}
 
 export default App
