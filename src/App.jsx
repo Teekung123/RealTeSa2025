b87fc934a4d067df8acad79db41c1fd71c870b6e
Angkii30 <angkinan30m@gmail.com>
2025-11-11 22:31:58 +0700

    เพม Rrports

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 import Map from './component/Map'
 import './App.css'
-import logo from './assets/Pic1.png'
-import avatar from './assets/Pic2.png'
 import { FiAlignJustify } from "react-icons/fi";
 import React from 'react'
 import Reports from './component/Reports.jsx'
+import Header from './component/Header.jsx'
 
 function App() {
   return (
     <>
-      <header className="top-bar">
-        <button className='menu-btn' aria-label='เปิดเมนู'>
-          <FiAlignJustify size={22} />
-        </button>
-
-        <img className="logo" src={logo} alt="Logo" />
-
-        <button className='profile-btn' aria-label='โปรไฟล์'>
-          <img src={avatar} alt="Profile" />
-        </button>
-        <div>Reports</div>
-      </header>
+        <Header />
       {/* <Map /> */}
     </>
   )
